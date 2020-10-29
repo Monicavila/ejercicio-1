4dbae823968bc6e64a69e1e4d132d97b57f1fbd3
Monicavila <di.monicavila@gmail.com>
2020-10-28 19:20:55 -0600

    add a padding

--- src/learn.js
+++ src/learn.js
@@ -4,7 +4,7 @@ import './styles.css';
 const Learn =() => {
     return(
         <div className="learn-container">
-            <h3>I'm gonna lear</h3>
+            <h3>I'm gonna learn</h3>
             <h6>Scrum methodology</h6>
             <h6>Coding better in Js and React</h6>
         </div>
